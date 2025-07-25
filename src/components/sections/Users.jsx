1684f7d56970c8c1c3461e0b52a59e46905039d9
muhammed <muhammedmamu2906@gmail.com>
2025-07-25 21:28:23 +0530

    bugs fixed

--- src/components/sections/Users.jsx
+++ src/components/sections/Users.jsx
@@ -1,5 +1,3 @@
-// components/sections/Users.jsx
-
 import React, { useState, useEffect, useMemo } from 'react';
 import { Search } from 'lucide-react';
 import DataTable from '../ui components/DataTable';
